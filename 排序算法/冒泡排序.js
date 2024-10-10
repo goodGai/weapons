e84668e8734a52d8a39a31572b223ec6eb7a6fba
进击的弗兰克 <1398281274@qq.com>
2024-10-10 17:42:53 +0800

    冒泡排序 # Reviewed, transaction id: 20175

---
+++ 排序算法/冒泡排序.js
@@ -0,0 +1,33 @@
+const arr = ['A', 'E', 'B', 'D', 'C'];
+
+// const bubleSort = (arr) => {
+//     let length = arr.length - 1;
+//     for (var i = 0; i < length; i++) {
+//         for (var j = 0; j < length - i ;j++) {
+//             if (arr[j] > arr[j+1]) {
+//                 const temp = arr[j];
+//                 arr[j] = arr[j+1];
+//                 arr[j+1] = temp;
+//             }
+//         }
+//     }
+//     return arr;
+// }
+
+const bubleSort = (arr) => {
+    let length = arr.length - 1;
+    while(length) {
+        for (var j = 0; j < length ;j++) {
+            if (arr[j] > arr[j+1]) {
+                const temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+        length--
+    }
+    return arr;
+}
+
+
+console.log(bubleSort(arr))
